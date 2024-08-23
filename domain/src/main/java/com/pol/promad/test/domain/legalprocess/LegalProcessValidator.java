64e5f7bb70c708638e4f33195bc3daa12f7c2b27
Denis <denis.william@seasolutions.io>
2024-08-23 19:19:09 -0300

    chore: use wither patters on Error

--- domain/src/main/java/com/pol/promad/test/domain/legalprocess/LegalProcessValidator.java
+++ domain/src/main/java/com/pol/promad/test/domain/legalprocess/LegalProcessValidator.java
@@ -10,9 +10,24 @@ import java.util.Set;
 
 public class LegalProcessValidator extends Validator {
 
+    public static final String O_STATUS_DO_PROCESSO_NAO_PODE_SER_NULO = 
+            "O status do processo não pode ser nulo";
+    public static final String O_STATUS_DO_PROCESSO_NAO_PODE_SER_VAZIO = 
+            "O status do processo não pode ser vazio";
+    public static final String O_STATUS_DO_PROCESSO_NAO_E_VÁLIDO = 
+            "O status do processo não é válido";
+    public static final String O_NUMERO_DO_PROCESSO_NAO_PODE_SER_NULO = 
+            "O número do processo não pode ser nulo";
+    public static final String O_NUMERO_DO_PROCESSO_NAO_PODE_SER_VAZIO = 
+            "O número do processo não pode ser vazio";
+    public static final String O_NUMERO_DO_PROCESSO_NAO_PODE_TER_MAIS_DE_25_CARACTERES = 
+            "O número do processo não pode ter mais de 25 caracteres";
+    public static final String D_7_D_2_D_4_D_D_2_D_4 = "\\d{7}-\\d{2}\\.\\d{4}\\.\\d\\.\\d{2}\\.\\d{4}";
+    public static final String O_NUMERO_DO_PROCESSO_NAO_RESPEITA_O_FORMATO_CORRETO_NNNNNNN_DD_AAAA_J_TR_OOOO = 
+            "O número do processo não respeita o formato correto 'NNNNNNN-DD.AAAA.J.TR.OOOO'";
+    public static final List<String> STATUS = 
+            List.of("EM_ANDAMENTO", "SUSPENSO", "FINALIZADO", "ARQUIVADO");
     private final LegalProcess legalProcess;
-    public static final List<String> STATUS = List.of("EM_ANDAMENTO", "SUSPENSO", "FINALIZADO", "ARQUIVADO");
-
     private static final Map<String, Set<String>> ALLOWED_TRANSITIONS = Map.of(
             "EM_ANDAMENTO", Set.of("SUSPENSO", "FINALIZADO"),
             "SUSPENSO", Set.of("EM_ANDAMENTO", "ARQUIVADO"),
@@ -35,33 +50,34 @@ public class LegalProcessValidator extends Validator {
 
     private void checkStatus() {
         if (this.legalProcess.getStatus().getValue().equals("error")) {
-            getHandler().append(new Error("O status do processo não pode ser nulo"));
+            getHandler().append(Error.with(O_STATUS_DO_PROCESSO_NAO_PODE_SER_NULO));
             return;
         }
         if (this.legalProcess.getStatus().getValue().isBlank()) {
-            getHandler().append(new Error("O status do processo não pode ser vazio"));
+            getHandler().append(Error.with(O_STATUS_DO_PROCESSO_NAO_PODE_SER_VAZIO));
             return;
         }
         if(!STATUS.contains(this.legalProcess.getStatus().getValue())){
-            getHandler().append(new Error("O status do processo não é válido"));
+            getHandler().append(Error.with(O_STATUS_DO_PROCESSO_NAO_E_VÁLIDO));
         }
     }
 
     private void checkNumber() {
         if (this.legalProcess.getNumber() == null) {
-            getHandler().append(new Error("O número do processo não pode ser nulo"));
+            getHandler().append(Error.with(O_NUMERO_DO_PROCESSO_NAO_PODE_SER_NULO));
             return;
         }
         if (this.legalProcess.getNumber().isBlank()) {
-            getHandler().append(new Error("O número do processo não pode ser vazio"));
+            getHandler().append(Error.with(O_NUMERO_DO_PROCESSO_NAO_PODE_SER_VAZIO));
             return;
         }
         if (this.legalProcess.getNumber().length() > 25) {
-            getHandler().append(new Error("O número do processo não pode ter mais de 25 caracteres"));
+            getHandler().append(Error.with(O_NUMERO_DO_PROCESSO_NAO_PODE_TER_MAIS_DE_25_CARACTERES));
             return;
         }
-        if (!this.legalProcess.getNumber().matches("\\d{7}-\\d{2}\\.\\d{4}\\.\\d\\.\\d{2}\\.\\d{4}")) {
-            getHandler().append(new Error("O número do processo não respeita o formato correto 'NNNNNNN-DD.AAAA.J.TR.OOOO'"));
+        if (!this.legalProcess.getNumber().matches(D_7_D_2_D_4_D_D_2_D_4)) {
+            getHandler().append(Error.with(
+                    O_NUMERO_DO_PROCESSO_NAO_RESPEITA_O_FORMATO_CORRETO_NNNNNNN_DD_AAAA_J_TR_OOOO));
         }
     }
 
@@ -70,26 +86,25 @@ public class LegalProcessValidator extends Validator {
             String currentStatus = this.legalProcess.getStatus().getValue();
             String newStatus = this.legalProcess.getPendingStatus().getValue();
             if (this.legalProcess.getPendingStatus().getValue().equals("error")) {
-                getHandler().append(new Error("O novo status do processo não pode ser nulo"));
+                getHandler().append(Error.with("O novo status do processo não pode ser nulo"));
                 return;
             }
             if (this.legalProcess.getPendingStatus().getValue().isBlank()) {
-                getHandler().append(new Error("O novo status do processo não pode ser vazio"));
+                getHandler().append(Error.with("O novo status do processo não pode ser vazio"));
                 return;
             }
             if(!STATUS.contains(this.legalProcess.getPendingStatus().getValue())){
-                getHandler().append(new Error("O novo status do processo não é válido"));
+                getHandler().append(Error.with("O novo status do processo não é válido"));
                 return;
             }
             if (!this.canTransition(currentStatus, newStatus)) {
-                getHandler().append(new Error("Transição de status inválida de " + currentStatus + " para " + newStatus));
+                getHandler()
+                        .append(Error.with("Transição de status inválida de " + currentStatus + " para " + newStatus));
             }
         }
     }
 
-    public static boolean canTransition(String currentStatus, String newStatus) {
+    private static boolean canTransition(String currentStatus, String newStatus) {
         return ALLOWED_TRANSITIONS.getOrDefault(currentStatus, Set.of()).contains(newStatus);
     }
-
-
 }
