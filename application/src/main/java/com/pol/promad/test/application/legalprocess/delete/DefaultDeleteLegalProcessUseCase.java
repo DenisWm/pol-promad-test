29bcdb004a6357fda525c8ef0acf5332d7865ef0
Denis <denis.william@seasolutions.io>
2024-08-24 10:19:26 -0300

    chore: implemented DefaultDeleteLegalProcessUseCase

---
+++ application/src/main/java/com/pol/promad/test/application/legalprocess/delete/DefaultDeleteLegalProcessUseCase.java
@@ -0,0 +1,21 @@
+package com.pol.promad.test.application.legalprocess.delete;
+
+import com.pol.promad.test.application.utils.IDNotFoundUtils;
+import com.pol.promad.test.domain.legalprocess.LegalProcess;
+import com.pol.promad.test.domain.legalprocess.LegalProcessGateway;
+import com.pol.promad.test.domain.legalprocess.LegalProcessID;
+
+import java.util.Objects;
+
+public class DefaultDeleteLegalProcessUseCase extends DeleteLegalProcessUseCase {
+
+    private final LegalProcessGateway legalProcessGateway;
+
+    public DefaultDeleteLegalProcessUseCase(final LegalProcessGateway legalProcessGateway) {
+        this.legalProcessGateway = Objects.requireNonNull(legalProcessGateway);
+    }
+    @Override
+    public void execute(final String anId) {
+        this.legalProcessGateway.deleteById(LegalProcessID.from(anId));
+    }
+}
